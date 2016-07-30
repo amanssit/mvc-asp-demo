4de4688cb767654639b3c50c3d8df5d6a1b0d49b
Aman Kumar <amanssit@gmail.com>
2016-07-30 21:46:03 +0530

    added employee MVC

--- mvcdemo2/App_Start/RouteConfig.cs
+++ mvcdemo2/App_Start/RouteConfig.cs
@@ -12,6 +12,13 @@ namespace mvcdemo2
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                "EmployeeRouter",
+                "Employee/Edit/{id}/{name}",
+            new { controller="Employee",action="Edit"},
+            new { id=@"\d{4}"}
+
+                );
 
             routes.MapRoute(
                 name: "Default",
---
+++ mvcdemo2/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using mvcdemo2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+           var employeeList = new List<Employee> { 
+            new Employee{Id=1,Name="Aman KUmar",Age=27},
+            new Employee{Id=2,Name="Anil KUmar ",Age=31},
+            new Employee{Id=3,Name="Vikas",Age=23},
+            new Employee{Id=4,Name="Amit KUmar ",Age=20}           
+            };
+
+           
+            return View(employeeList);
+        }
+
+        public ActionResult Edit(int id,string name)
+        {
+            var emp = new Employee { Id = 2, Name = "Anil KUmar", Age = 31 };
+            return View(emp);
+        }
+        public ActionResult Delete(int id)
+        {
+            
+            return View();
+        }
+	}
+}
